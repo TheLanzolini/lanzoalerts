2edff9a432ae86f36d99e1c04d0e73b834415b37
Alex <thelanzolini@gmail.com>
2017-07-05 21:31:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+// Set up a URL route
+app.get("/", function(req, res) {
+  res.send("Heroku Demo!");
+});
+
+// bind the app to listen for connections on a specified port
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+// Render some console log output
+console.log("Listening on port " + port);
