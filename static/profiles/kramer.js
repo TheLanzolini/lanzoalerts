d1f4668a311287b374414c2defee305cdace98e7
Alex <thelanzolini@gmail.com>
2017-11-20 21:46:44 -0500

    KRAMER

--- static/profiles/kramer.js
+++ static/profiles/kramer.js
@@ -2,11 +2,12 @@ var ROOM = 'thelanzolini';
 var QUEUE_INTERVAL_TIME = 30000;
 
 if(location.search == '?test'){
+  QUEUE_INTERVAL_TIME = 1000;
   var QUEUE = [
     { type: 'follow', data: { user: { display_name: "ThePoridgeater", name: "theporidgeater" } } },
-    { type: 'follow', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } },
-    { type: 'follow', data: { user: { display_name: "Afeonim", name: "afeonim" } } },
-    { type: 'follow', data: { user: { display_name: "Coreyshift", name: "coreyshift" } } }
+    // { type: 'follow', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } },
+    // { type: 'follow', data: { user: { display_name: "Afeonim", name: "afeonim" } } },
+    // { type: 'follow', data: { user: { display_name: "Coreyshift", name: "coreyshift" } } }
   ]
 } else {
   var QUEUE = [];
@@ -21,12 +22,18 @@ window.addEventListener('DOMContentLoaded', function(){
   $theme.src = '/sounds/kramer/seinfeld.mp3';
   $theme.volume = 0.5;
 
+  var link = document.createElement('link');
+  link.rel = "stylesheet";
+  link.href = "https://fonts.googleapis.com/css?family=Open+Sans";
+  document.head.appendChild(link);
+
   var socket = io(`/${ROOM}`);
   socket.on('init', function(data){
     console.log(data);
   });
   socket.on('subscription', function(data){
     console.log(data);
+    QUEUE.push({ type: 'follow', data });
   });
   socket.on('resub', function(data){
     console.log(data);
@@ -59,28 +66,50 @@ function notify(notification) {
 
   $theme.play();
 
-  var $userName = document.createElement('div');
-  $userName.classList.add('user-name');
-  var $userText = document.createElement('div');
-  $userText.classList.add('text');
-  $userText.textContent = notification.data.user.display_name;
-  $userName.appendChild($userText);
+  var $sein = document.createElement('div');
+  $sein.classList.add('sein');
+  var $seinText = document.createElement('div');
+  $seinText.classList.add('sein-text');
+  // $seinText.textContent = notification.data.user.display_name;
+  $seinText.textContent = 'TheLanzolini';
+  $sein.appendChild($seinText);
 
-  var $userDescription = document.createElement('div');
-  $userDescription.classList.add('description');
-  $userDescription.textContent = 'New Follower';
+  var $user = document.createElement('div');
+  $user.classList.add('user');
 
-  $notification.appendChild($userName);
-  $notification.appendChild($userDescription);
+  var $description = document.createElement('div');
+  $description.classList.add('description');
+  $description.textContent = 'New Follower';
+
+  var $name = document.createElement('div');
+  $name.classList.add('user-name');
+  $name.textContent = notification.data.user.display_name;
+
+  $user.appendChild($description);
+  $user.appendChild($name);
+
+  var $kramerWrapper = document.createElement('div');
+  $kramerWrapper.classList.add('kramer-wrapper');
+
+  var $kramer = document.createElement('img');
+  $kramer.src = 'https://i.imgur.com/Z4kYED1.gif';
+
+  $kramerWrapper.appendChild($kramer);
+
+  $notification.appendChild($sein);
+  $notification.appendChild($user);
+  $notification.appendChild($kramerWrapper);
 
   setTimeout(function(){
     $notification.classList.add('show');
   }, 1000);
 
 
-
+  setTimeout(function(){
+    $notification.classList.remove('show');
+  }, 25000);
 
   setTimeout(function(){
     $notification.innerHTML = '';
-  }, 25000);
+  }, 26000);
 }
