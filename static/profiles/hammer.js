20b690199d9caff46d127e91e3b0d980db98eeeb
TheLanzolini <thelanzolini@gmail.com>
2017-12-19 23:05:52 -0500

    remove summit, finish hammer

--- static/profiles/hammer.js
+++ static/profiles/hammer.js
@@ -5,10 +5,10 @@ var QUEUE_INTERVAL_TIME = 30000;
 if(location.search == '?test'){
   QUEUE_INTERVAL_TIME = 1000;
   var QUEUE = [
-    { type: 'follow', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } }
-    // { type: 'cheer', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" }, userstate: { bits: 100 } } },
-    // { type: 'resub', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } },
-    // { type: 'subscription', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } }
+    { type: 'cheer', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" }, userstate: { bits: 100 } } },
+    { type: 'follow', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } },
+    { type: 'resub', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } },
+    { type: 'subscription', data: { user: { display_name: "TheLanzolini", name: "thelanzolini" } } }
   ]
 } else {
   var QUEUE = [];
@@ -16,21 +16,25 @@ if(location.search == '?test'){
 
 var $notification, $theme;
 var descriptionMap = {
-  follow: 'New Follower',
-  subscription: 'New Subscriber',
-  resub: 'Executive Subscriber',
-  cheer: 'Brought to you with %b Bits'
+  follow: 'New Follower!',
+  subscription: 'New Subscriber!',
+  resub: 'New Resub!',
+  cheer: 'Cheered %b!'
 }
 
 window.addEventListener('DOMContentLoaded', function(){
   $notification = document.getElementById('notification');
-  // $theme = document.createElement('audio');
-  // $theme.src = '/sounds/kramer/seinfeld.mp3';
-  // $theme.volume = 0.5;
+  $sound = document.createElement('audio');
+  $sound.src = '/sounds/hammer/sonicboom.mp3';
+  $sound.volume = 0.5;
+
+  $hammerSound = document.createElement('audio');
+  $hammerSound.src = '/sounds/hammer/hammer.mp3';
+  $hammerSound.volume = 0.5;
 
   var link = document.createElement('link');
   link.rel = "stylesheet";
-  link.href = "https://fonts.googleapis.com/css?family=Open+Sans";
+  link.href = "https://fonts.googleapis.com/css?family=Bungee";
   document.head.appendChild(link);
 
   var socket = io(`/${ROOM}`);
@@ -65,6 +69,11 @@ window.addEventListener('DOMContentLoaded', function(){
 function notify(notification) {
   console.log(notification);
   var $notificationBody = document.createElement('div');
+  var $explosion = document.createElement('img');
+  $explosion.src = '/images/explosion.gif';
+  $explosion.classList.add('explosion');
+  $sound.play();
+  $hammerSound.play();
 
   var mjolnir1 = document.createElement('img');
   mjolnir1.src = '/images/hammer/mjolnir.png';
@@ -76,6 +85,38 @@ function notify(notification) {
   $notificationBody.appendChild(mjolnir1);
   $notificationBody.appendChild(mjolnir2);
 
+  var $notificationTextWrapper = document.createElement('div');
+  var $notificationTextLabel = document.createElement('div');
+  var $notificationTextValue = document.createElement('div');
+
+  $notificationTextLabel.textContent = descriptionMap[notification.type].replace('%b', (notification.data.userstate || {bits: ''}).bits)
+  $notificationTextValue.textContent = notification.data.user.display_name;
+
+  $notificationTextWrapper.appendChild($notificationTextLabel);
+  $notificationTextWrapper.appendChild($notificationTextValue);
+  $notificationTextWrapper.classList.add('notification-text');
+
   $notification.appendChild($notificationBody);
 
+  setTimeout(function(){
+    $notificationBody.appendChild($explosion);
+    $notification.appendChild($notificationTextWrapper);
+  }, 2100);
+
+  setTimeout(function(){
+    $notificationTextWrapper.classList.add('fade');
+  }, 2800);
+
+  setTimeout(function(){
+    $explosion.classList.add('fade');
+  }, 2800);
+
+
+  setTimeout(function(){
+    $notificationTextWrapper.classList.remove('fade');
+  }, 20000);
+
+  setTimeout(function(){
+    $notification.innerHTML = '';
+  }, 26000);
 }
