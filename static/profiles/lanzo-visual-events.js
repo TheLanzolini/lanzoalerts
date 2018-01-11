5c4eeaeabc026249bb318a6b596ac0d2e66fd584
TheLanzolini <thelanzolini@gmail.com>
2018-01-10 19:33:58 -0500

    added sweetvictory

--- static/profiles/lanzo-visual-events.js
+++ static/profiles/lanzo-visual-events.js
@@ -1,6 +1,7 @@
 var PROFILE = 'lanzo-visual-commands';
 var ROOM = 'lanzo';
 var running = false;
+var $notification, $sweetvictory;
 
 if(location.search == '?test'){
   var QUEUE = [
@@ -10,6 +11,8 @@ if(location.search == '?test'){
 } else {
   var QUEUE = [];
 }
+
+
 
 function next() {
   if(QUEUE[0]) {
@@ -36,6 +39,7 @@ function video(name) {
     var $video = document.createElement('video');
     $video.src = '/videos/'+ name +'.mp4';
     $video.setAttribute('autoplay', true);
+    $video.volume = 0.8;
     document.body.appendChild($video);
     $video.addEventListener('ended', function(){
       document.body.removeChild($video);
@@ -56,15 +60,51 @@ function theway() {
   return video('theway');
 }
 
+function sweetvictory() {
+  return new Promise(function(resolve, reject) {
+    var $spongeBody = document.createElement('div');
+    $spongeBody.classList.add('sponge-body');
+    $notification.appendChild($spongeBody);
+
+    var members = ['krabs', 'patrick', 'sandy', 'spongebob'];
+    members.forEach(function(member){
+      var iWrap = document.createElement('div');
+      iWrap.classList.add('rockstar-wrap', `${member}-wrap`);
+      var i = document.createElement('img');
+      i.src = `/images/sweet_victory/${member}.jpg`;
+      i.classList.add(member, 'rockstar');
+      iWrap.appendChild(i)
+      $spongeBody.appendChild(iWrap);
+    });
+
+    var fans = document.createElement('img');
+    fans.classList.add('fans');
+    fans.src = '/images/sweet_victory/giphy.gif';
+
+    $spongeBody.appendChild(fans);
+
+    $sweetVictory.play();
+
+    setTimeout(function(){
+      $notification.innerHTML = '';
+      resolve();
+    }, 9000);
+  });
+}
+
 var commands = {
   '!jacked': jacked,
   '!praise': praise,
-  '!theway': theway
+  '!theway': theway,
+  '!sweetvictory': sweetvictory
 }
 
 window.addEventListener('DOMContentLoaded', function(){
   var socket = io(`/${ROOM}`);
-
+  $sweetVictory = document.createElement('audio');
+  $sweetVictory.src = '/sounds/thelanzolini/sweet_victory.mp3';
+  $sweetVictory.volume = 0.5;
+  $notification = document.getElementById('notification');
   socket.on('command', function(data) {
     console.log(data);
     if (commands[data.command]) {
